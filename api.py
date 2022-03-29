e4e9eb9b3c82a7d56addca49ab7a202b45372853
Leo-Aug <57796691+Leo-Aug@users.noreply.github.com>
2022-03-29 10:17:53 +0000

    添加比较文件相似度的api

--- api.py
+++ api.py
@@ -1,10 +1,14 @@
+from telnetlib import DO
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
 
 from pydantic import BaseModel
 
 import uvicorn
+
+from docx import Document
+from pdfminer.high_level import extract_text
 
 from text_compare import compare_text
 
@@ -17,6 +21,27 @@ app = FastAPI()
 @app.post("/checktext")
 def check_cheat(texts: TextData):
     return compare_text(texts.text1, texts.text2)
+
+def parse_text(file: UploadFile) -> Optional[str]:
+    """
+    从不同的文件中解析文本
+    """
+    if(file.filename.endswith(".docx")):
+        document = Document(file.file._file)
+        text = ""
+        for paragraph in document.paragraphs:
+            text += paragraph.text
+        return text
+    elif(file.filename.endswith(".pdf")):
+        text = extract_text(file.file._file)
+        return text
+
+@app.post("/checkfile")
+def check_cheat(file1: UploadFile, file2: UploadFile):
+    """
+    比较两个文件的相似度
+    """
+    return compare_text(parse_text(file1), parse_text(file2))
 
 
 if __name__ == "__main__":
