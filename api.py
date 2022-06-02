1b57cf4376a2ff479b3001f4dc1c1143c3a41c31
Leo-Aug <57796691+Leo-Aug@users.noreply.github.com>
2022-06-02 04:08:36 +0000

    端口改为80

--- api.py
+++ api.py
@@ -82,4 +82,4 @@ app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 
 if __name__ == "__main__":
-    uvicorn.run(app="api:app", host="127.0.0.1", port=8000)
+    uvicorn.run(app="api:app", host="0.0.0.0", port=80)
